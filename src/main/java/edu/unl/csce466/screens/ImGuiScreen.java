2cb4d16033f68a3e00884139b672e3cb983a2221
Timothy Gibbons <tjgibbons10@gmail.com>
2026-01-27 12:34:01 -0600

    feat: Added a simple window with a button

--- src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
+++ src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
@@ -11,6 +11,8 @@ import net.minecraft.network.chat.Component;
 public class ImGuiScreen extends Screen{
 	
 	private static ImGuiScreen _INSTANCE = null;
+
+	boolean _buttonClicked = false;
 	
 	public static ImGuiScreen getInstance() {
 		if(_INSTANCE == null) { _INSTANCE = new ImGuiScreen(); }
@@ -27,6 +29,19 @@ public class ImGuiScreen extends Screen{
 	public void render(PoseStack proseStack, int x, int y, float partialTicks) {
 		ImGuiRenderer.getInstance().draw(()->{
 			ImGui.showDemoWindow();
+		});
+		
+		ImGuiRenderer.getInstance().draw(() -> {
+			ImGui.begin("Custom Window");
+				ImGui.text("Example WIndow");
+				if(ImGui.button("Click Me!")) {
+					_buttonClicked = true;
+				}
+				
+				if(_buttonClicked) {
+					ImGui.text("Button has been clicked!");
+				}
+			ImGui.end();
 		});
 	}
 }
