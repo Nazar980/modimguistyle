6d59a5e3861efe3c81a67d7ab25adad12186a788
Nazar980 <golovysiaut5@gmail.com>
2026-01-28 20:04:50 +0300

    Update ImGuiScreen.java

--- src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
+++ src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
@@ -3,6 +3,8 @@ package edu.unl.csce466.screens;
 import com.mojang.blaze3d.vertex.PoseStack;
 import edu.unl.csce466.imgui.ImGuiRenderer;
 import imgui.ImGui;
+import imgui.ImGuiIO;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
@@ -25,6 +27,11 @@ public class ImGuiScreen extends Screen {
 
     @Override
     public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+        // Обновляем позицию мыши каждый кадр
+        ImGuiIO io = ImGui.getIO();
+        Minecraft mc = Minecraft.getInstance();
+        io.setMousePos((float) mc.mouseHandler.xpos(), (float) mc.mouseHandler.ypos());
+
         ImGuiRenderer.getInstance().draw(() -> {
             ImGui.begin("ImGui Example");
             ImGui.text("Minecraft 1.19.2 + ImGui");
@@ -39,5 +46,33 @@ public class ImGuiScreen extends Screen {
 
             ImGui.end();
         });
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        ImGui.getIO().setMouseDown(button, true);
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        ImGui.getIO().setMouseDown(button, false);
+        return super.mouseReleased(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button) {
+        return super.mouseDragged(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+        ImGui.getIO().addMouseWheelDelta(0, (float) delta);
+        return super.mouseScrolled(mouseX, mouseY, delta);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
 }
