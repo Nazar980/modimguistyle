a3010ccf659d4433f60d02609e1da71656ad0f5a
Nazar980 <golovysiaut5@gmail.com>
2026-01-28 19:48:52 +0300

    Update ImGuiScreen.java

--- src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
+++ src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
@@ -1,7 +1,6 @@
 package edu.unl.csce466.screens;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import edu.unl.csce466.ExampleMod;
 import edu.unl.csce466.imgui.ImGuiRenderer;
 import imgui.ImGui;
 import net.minecraft.client.gui.screens.Screen;
@@ -10,7 +9,7 @@ import net.minecraft.network.chat.Component;
 public class ImGuiScreen extends Screen {
 
     private static ImGuiScreen _INSTANCE = null;
-    private boolean _buttonClicked = false;
+    private boolean buttonClicked = false;
 
     public static ImGuiScreen getInstance() {
         if (_INSTANCE == null) _INSTANCE = new ImGuiScreen();
@@ -22,62 +21,23 @@ public class ImGuiScreen extends Screen {
     }
 
     public void init() {
-        // можно оставить пустым или добавить что-то при необходимости
     }
 
     @Override
     public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
-        // фон по желанию (полупрозрачный или чёрный)
-        // this.renderBackground(poseStack);
-
         ImGuiRenderer.getInstance().draw(() -> {
-            ShowModMenu();
-        });
+            ImGui.begin("ImGui Example");
+            ImGui.text("Minecraft 1.19.2 + ImGui");
 
-        // Убрал дублирующееся окно "Custom Window" — было два одинаковых
-        ImGuiRenderer.getInstance().draw(() -> {
-            ImGui.begin("Example Window");
-            ImGui.text("Hello from ImGui in Minecraft 1.19.2");
+            if (ImGui.button("Click me")) {
+                buttonClicked = true;
+            }
 
-            if (ImGui.button("Click me!")) {
-                _buttonClicked = true;
+            if (buttonClicked) {
+                ImGui.text("Button clicked!");
             }
-            if (_buttonClicked) {
-                ImGui.text("Button was clicked!");
-            }
+
             ImGui.end();
         });
-    }
-
-    private void ShowModMenu() {
-        ExampleMod.Zeus zeus = new ExampleMod.Zeus();
-
-        if (ImGui.beginMenu("Level Up")) {
-            if (ImGui.menuItem("Level Up!")) {
-                zeus.LevelUp();
-            }
-            ImGui.endMenu();
-        }
-
-        if (ImGui.beginMenu("Health")) {
-            if (ImGui.menuItem("Health boost")) {
-                zeus.Health();
-            }
-            ImGui.endMenu();
-        }
-
-        if (ImGui.beginMenu("Diamonds")) {
-            if (ImGui.menuItem("Give Diamonds")) {
-                zeus.GiveDiamonds();
-            }
-            ImGui.endMenu();
-        }
-
-        if (ImGui.beginMenu("Stick")) {
-            if (ImGui.menuItem("Stick!")) {
-                zeus.Stick();
-            }
-            ImGui.endMenu();
-        }
     }
 }
