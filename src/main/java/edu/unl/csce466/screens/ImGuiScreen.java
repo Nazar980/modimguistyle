c7180ac669b62895fa009c8bbd01e8d16b54c603
Nazar980 <golovysiaut5@gmail.com>
2026-01-28 19:38:12 +0300

    Update ImGuiScreen.java

--- src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
+++ src/main/java/edu/unl/csce466/screens/ImGuiScreen.java
@@ -1,103 +1,83 @@
 package edu.unl.csce466.screens;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-
+import edu.unl.csce466.ExampleMod;
 import edu.unl.csce466.imgui.ImGuiRenderer;
 import imgui.ImGui;
-import imgui.flag.ImGuiConfigFlags;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import edu.unl.csce466.ExampleMod;
 
-public class ImGuiScreen extends Screen{
-	
-	private static ImGuiScreen _INSTANCE = null;
+public class ImGuiScreen extends Screen {
 
-	boolean _buttonClicked = false;
-	
-	public static ImGuiScreen getInstance() {
-		if(_INSTANCE == null) { _INSTANCE = new ImGuiScreen(); }
-		return _INSTANCE;
-	}
+    private static ImGuiScreen _INSTANCE = null;
+    private boolean _buttonClicked = false;
 
-	private ImGuiScreen() {
-		super(Component.literal("ImGui"));
-	}
-	
-	public void init() {
-	}
-	
-	public void render(PoseStack proseStack, int x, int y, float partialTicks) {
-		ImGuiRenderer.getInstance().draw(()->{
-			//ImGui.showDemoWindow();
-			ShowModMenu();
-		});
-		
-		ImGuiRenderer.getInstance().draw(() -> {
-			ImGui.begin("Custom Window");
-				ImGui.text("Example WIndow");
-				if(ImGui.button("Click Me!")) {
-					_buttonClicked = true;
-				}
-				
-				if(_buttonClicked) {
-					ImGui.text("Button has been clicked!");
-				}
-			ImGui.end();
-		});
-		
-		ImGuiRenderer.getInstance().draw(() -> {
-			ImGui.begin("Custom Window");
-				ImGui.text("Example WIndow");
-				if(ImGui.button("Click Me!")) {
-					_buttonClicked = true;
-				}
-				
-				if(_buttonClicked) {
-					ImGui.text("Button has been clicked!");
-				}
-			ImGui.end();
-		});
-	}
+    public static ImGuiScreen getInstance() {
+        if (_INSTANCE == null) _INSTANCE = new ImGuiScreen();
+        return _INSTANCE;
+    }
 
-	private void ShowModMenu()
-	{
-		// init zeus class
-		ExampleMod.Zeus zeus = new ExampleMod.Zeus();
+    private ImGuiScreen() {
+        super(Component.literal("ImGui"));
+    }
 
-		//zeus menu
-		//if (ImGui.beginMenu("Zeus")) {
-		//	ImGui.text("Channel your inner thunder!");
-		//	if (ImGui.button("Activate"))
-		//	{
-		//		zeus.Init();
-		//	}
-		//	ImGui.endMenu();
-		//}
+    public void init() {
+        // можно оставить пустым или добавить что-то при необходимости
+    }
 
-		if (ImGui.beginMenu("Level Up"))
-		{
-			if (ImGui.button("Level Up!")) { zeus.LevelUp();}
-			ImGui.endMenu();
-		}
+    @Override
+    public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
+        // фон по желанию (полупрозрачный или чёрный)
+        // this.renderBackground(poseStack);
 
-		if (ImGui.beginMenu("Health"))
-		{
-			if (ImGui.button("Health boost")) { zeus.Health();}
-			ImGui.endMenu();
-		}
+        ImGuiRenderer.getInstance().draw(() -> {
+            ShowModMenu();
+        });
 
-		if (ImGui.beginMenu("Diamonds"))
-		{
-			if (ImGui.button("Give Diamonds")) { zeus.GiveDiamonds();}
-			ImGui.endMenu();
-		}
+        // Убрал дублирующееся окно "Custom Window" — было два одинаковых
+        ImGuiRenderer.getInstance().draw(() -> {
+            ImGui.begin("Example Window");
+            ImGui.text("Hello from ImGui in Minecraft 1.19.2");
 
-		if (ImGui.beginMenu("Stick"))
-		{
-			if (ImGui.button("Stick!")) { zeus.Stick();}
-			ImGui.endMenu();
-		}
+            if (ImGui.button("Click me!")) {
+                _buttonClicked = true;
+            }
+            if (_buttonClicked) {
+                ImGui.text("Button was clicked!");
+            }
+            ImGui.end();
+        });
+    }
 
-	}
+    private void ShowModMenu() {
+        ExampleMod.Zeus zeus = new ExampleMod.Zeus();
+
+        if (ImGui.beginMenu("Level Up")) {
+            if (ImGui.menuItem("Level Up!")) {
+                zeus.LevelUp();
+            }
+            ImGui.endMenu();
+        }
+
+        if (ImGui.beginMenu("Health")) {
+            if (ImGui.menuItem("Health boost")) {
+                zeus.Health();
+            }
+            ImGui.endMenu();
+        }
+
+        if (ImGui.beginMenu("Diamonds")) {
+            if (ImGui.menuItem("Give Diamonds")) {
+                zeus.GiveDiamonds();
+            }
+            ImGui.endMenu();
+        }
+
+        if (ImGui.beginMenu("Stick")) {
+            if (ImGui.menuItem("Stick!")) {
+                zeus.Stick();
+            }
+            ImGui.endMenu();
+        }
+    }
 }
